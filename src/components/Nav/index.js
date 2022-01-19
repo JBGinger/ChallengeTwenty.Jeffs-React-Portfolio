fd0f580c4f6a4a901b380cfc613aafa39fd84014
jeffb <jeffb@bsdiweb.com>
2022-01-19 12:57:35 -0500

    Started setting up nav bar

---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Nav() {
+    return (
+      <header>
+          <h2>
+              <a data-testid="link" href="/">
+                  Jeffrey Brittingham
+              </a>
+          </h2>
+          <nav>
+              <ul className='flex-row'>
+                  <li className='mx-2'>
+                      <a href='#about'>
+                          About Me
+                      </a>
+                  </li>
+                  <li>
+                      <span>Contact</span>
+                  </li>
+              </ul>
+          </nav>
+      </header>
+    );
+  }
+  
+  export default Nav;
