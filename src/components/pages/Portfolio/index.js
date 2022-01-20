9c6d929b3c6bf6bdc0ee61925f3030a2b5066d88
jeffb <jeffb@bsdiweb.com>
2022-01-20 12:36:43 -0500

    Fixed portfolio images, removed json file

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -1,32 +1,38 @@
 import React, { useState } from 'react';
 import ProjectCard from './projectcard';
-import projects from './projects.json';
-
 import Astronaut from '../../../assets/portfolio-images/Astronaut.jpg';
+import Screenshot from '../../../assets/portfolio-images/Project.png';
 
 
 
 function Portfolio() {
     const projectList = [
         {
-            id: projects.id,
-            name: projects.name,
-            image: Astronaut,
-            applink: projects.applink,
-            githublink: projects.githublink
+            "id": 1,
+            "name": "Space-RPG",
+            "image": Astronaut,
+            "applink": "https://quiet-mountain-05103.herokuapp.com/",
+            "githublink": "https://github.com/JBGinger/Space-RPG"
+        },
+        {
+            "id": 2,
+            "name": "Kanye-vs-Ron",
+            "image": Screenshot,
+            "applink": "https://jbginger.github.io/Group1.Module7Project/",
+            "githublink": "https://github.com/JBGinger/Group1.Module7Project"
         }
     ]
 
     return (
         <div id="portfolio">
             <h1 id="portfolio_title">Portfolio</h1>
-            {projectList.map((projects) => (
-                <ProjectCard
-                    id={projects.id}
-                    name={projects.name}
-                    image={projects.image}
-                    applink={projects.applink}
-                    githublink={projects.githublink}
+            { projectList.length > 0 && projectList.map((projectList) => (
+                <ProjectCard key={projectList.id}
+                    id={projectList.id}
+                    name={projectList.name}
+                    image={projectList.image}
+                    applink={projectList.applink}
+                    githublink={projectList.githublink}
                 />
             ))}
         </div>
