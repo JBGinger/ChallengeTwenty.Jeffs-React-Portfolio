d143b498da0b88995ccc0a48deb45275c01c91b8
jeffb <jeffb@bsdiweb.com>
2022-01-20 22:27:23 -0500

    Updated css styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import PageContainer from "./components/PageContainer";
+import Footer from "./components/Footer";
 
-const App = () => <PageContainer />;
+const App = () => (
+    <div>
+        <PageContainer />
+        <Footer />
+    </div>
+);
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Github from '../assets/misc-images/github.png';
+import LinkedIn from '../assets/misc-images/linkedin.png';
+import Stackoverflow from '../assets/misc-images/stack-overflow.png'
 
 const Footer = () => (
-    <div>
-        
-    </div>
+    <footer>
+        <section>
+            <div>
+                <a href='https://github.com/JBGinger'>
+                    <img src={Github} alt="Github logo" />
+                </a>
+            </div>
+            <div>
+                <a href='https://www.linkedin.com/'>
+                    <img src={LinkedIn} alt="LinkedIn logo" />
+                </a>
+            </div>
+            <div>
+                <a href='https://stackoverflow.com/'>
+                    <img src={Stackoverflow} alt="Stackoverflow logo" />
+                </a>
+            </div>
+        </section>
+    </footer>
 )
 
 export default Footer;
